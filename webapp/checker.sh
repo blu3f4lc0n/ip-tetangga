05944cfaf86403d2baa6ea58e630525c4c18bd4a
Ghozy Arif Fajri <gojigeje@gmail.com>
2014-05-20 16:24:16 +0700

    add: identifikasi layanan berdasarkan HTTP response code

--- webapp/checker.sh
+++ webapp/checker.sh
@@ -15,30 +15,78 @@ echo -n "" > "$dofile"
 echo -n "0" > "$donefile"
 
 sort_ip() {
-  sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 "$dofile" > "$dofile.tmp"
-  rm "$dofile" && mv "$dofile.tmp" "$dofile"
+  sort -n -t . -k 1,1 -k 2,2 -k 3,3 -k 4,4 "$dofile" > "$dofile.url"
+  rm "$dofile" && mv "$dofile.url" "$dofile"
 }
 
 linkify() {
-  cat "$dofile" | sed 's/^/<div class="col-xs-6 col-sm-4 col-md-3"><a class="btn btn-primary btn-block" href="http:\/\//g' | sed 's/$/\/" target="_blank">/g' > "$dofile.tmp"
-  paste "$dofile.tmp" "$dofile" | sed 's/$/<\/a><\/div>/g' > "$dofile.temp"
-  rm "$dofile" "$dofile.tmp"
-  mv "$dofile.temp" "$dofile"
+  cat "$dofile" | sed 's/^/<div class="col-xs-6 col-sm-4 col-md-3"><a class="btn btn-primary btn-block" href="http:\/\//g' | sed 's/$/\/" target="_blank">/g' > "$dofile.url"
+}
+
+combine() {
+  if [[ "$1" == "1" ]]; then
+    # 3 file
+    paste "$dofile.url" "$dofile" "$dofile.response" | sed 's/$/<\/a><\/div>/g' > "$dofile.temp"
+    rm "$dofile" "$dofile.url" "$dofile.response"
+    mv "$dofile.temp" "$dofile"
+    echo "<div id=\"legend\"> <p> Keterangan simbol:<br> <span class=\"label label-warning\"> &nbsp;<i class=\"fa fa-lock\"></i> </span> &nbsp;: IP diproteksi password (biasanya IP modem ADSL). <br> <span class=\"label label-success\"> <i class=\"fa fa-cloud\"></i> </span> &nbsp;: IP menjalankan web server, mikrotik atau server webcam. <br> <span class=\"label label-info\"> &nbsp;<i class=\"fa fa-question\"></i>&nbsp; </span> &nbsp;: Gagal mengidentifikasi layanan karena timeout, pastikan dengan mengaksesnya secara manual (klik). <br> </p> </div> <br>" | cat - "$dofile" > "$dofile.temp" && mv "$dofile.temp" "$dofile"
+    rm "$dofile.temp"
+  else
+    # 2 file
+    paste "$dofile.url" "$dofile" | sed 's/$/<\/a><\/div>/g' > "$dofile.temp"
+    rm "$dofile" "$dofile.url"
+    mv "$dofile.temp" "$dofile"
+  fi
+}
+
+identify() {
+  if [[ "$1" == "1" ]]; then
+    # identify
+    echo -n "" > "$dofile.response"
+    while read line; do
+      echo -n "Checking http://$line/ .. "
+      curl -o /dev/null --silent --head --write-out '%{http_code}' "http://$line/" > "response" &
+      CPID=$!
+      sleep 1
+      CPSPID=$(ps | grep $CPID | grep -v grep)
+      if [ "$CPSPID" != "" ]; then
+        # macet
+        echo "<span class=\"label label-info\"> <i class=\"fa fa-question\"></i> </span>" >> "$dofile.response"
+      else
+        response=$(cat "response")
+        if [[ "$response" == "200" ]]; then
+          echo "<span class=\"label label-success\"> <i class=\"fa fa-cloud\"></i> </span>" >> "$dofile.response"
+        else
+          if [[ "$response" == "401" ]]; then
+            echo "<span class=\"label label-warning\"> <i class=\"fa fa-lock\"></i> </span>" >> "$dofile.response"
+          else
+            echo " " >> "$dofile.response"
+          fi
+        fi
+      fi
+    done < "$dofile"
+
+    rm "response"
+  fi
 }
 
 fping -agq "$prefix.1" "$prefix.255" 2> /dev/null >> "$dofile" &
 
 PID=$!
-sleep 30
+sleep 15
 PSPID=$(ps | grep $PID | grep -v grep)
 if [ "$PSPID" != "" ]; then
   # macet
   kill $PID > /dev/null 2>&1
   sort_ip
   linkify
+  identify "$2"
+  combine "$2"
 else
   sort_ip
   linkify
+  identify "$2"
+  combine "$2"
 fi
 
 echo -n "done" > "$donefile"
